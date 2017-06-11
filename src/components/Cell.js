7a750b5a1c7c0f61d9049e5a3bba7535834546c6
Rikard Gehlin <rikardgehlin@gmail.com>
2017-06-11 23:10:32 +0200

    💩 Mess around with the representation of a cell

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,30 +1,146 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import React3 from "react-three-renderer";
-import { Euler, Vector3 } from "three";
+import { Color, Vector3, Euler } from "three";
 
 /**
- * The component representing the robot / main player. We can implement it as
+ * The component representing the individual cell of a map. We can implement it as
  * functional stateless component because it's so simple
  **/
-const Cell = ({ position, rotation, color, wireframe }) => (
-  <group position={position} rotation={rotation}>
-    {/*
+const Cell = props => {
+  let {
+    position,
+    rotation,
+    width,
+    height,
+    depth,
+    water,
+    fertility,
+    temperature,
+    growth
+  } = props;
+
+  const getWaterColor = water => {
+    return new Color(0x003366);
+  };
+
+  const getGrowthColor = growth => {
+    const color = new Color(0x005a04);
+    const colorHSL = color.getHSL();
+    console.log(
+      "aa",
+      colorHSL,
+      color.setHSL(colorHSL.h, colorHSL.s, colorHSL.l)
+    );
+    return color.setHSL(colorHSL.h, colorHSL.s, colorHSL.l * (1 - growth));
+  };
+
+  const getFertilityColor = fertility => {
+    return new Color(0x7f5417).multiplyScalar(2 - fertility);
+  };
+
+  const getTemperatureColor = temperature => {
+    /*  
+        temperature = white to orange,
+        coldest: 238, 238, 238
+        cold: 245, 250, 250
+        base color: 255, 255, 204
+        warm: 255, 204, 51
+        warmest: 255, 153, 0
+    */
+
+    let tempColor;
+    if (temperature <= -15) {
+      tempColor = new Color(0xeeeeee);
+    } else if (-15 < temperature && temperature <= 0) {
+      tempColor = new Color(0xf5fafa);
+    } else if (0 < temperature && temperature <= 15) {
+      tempColor = new Color(0xffffcc);
+    } else if (15 < temperature && temperature <= 30) {
+      tempColor = new Color(0xffcc33);
+    } else if (30 < temperature) {
+      tempColor = new Color(0xff9900);
+    } else {
+      tempColor = new Color(0xeeeeee);
+    }
+
+    return tempColor;
+  };
+
+  const shadeColor2 = (color, percent) => {
+    var f = parseInt(color, 16),
+      t = percent < 0 ? 0 : 255,
+      p = percent < 0 ? percent * -1 : percent,
+      R = f >> 16,
+      G = (f >> 8) & 0x00ff,
+      B = f & 0x0000ff;
+
+    const color2 =
+      "#" +
+      (0x1000000 +
+        (Math.round((t - R) * p) + R) * 0x10000 +
+        (Math.round((t - G) * p) + G) * 0x100 +
+        (Math.round((t - B) * p) + B))
+        .toString(16)
+        .slice(1);
+
+    return new Color(color2);
+  };
+
+  const getColor = (isWater, growth, temperature, fertility, position) => {
+    let color = new Color(0x8d8d8d);
+
+    // Pure water, ie lakes, rivers, oceans...
+    if (isWater === true) {
+      return getWaterColor(water);
+    }
+
+    // The more growth, the greener the cell
+    if (growth) {
+      color.multiply(getGrowthColor(growth));
+    }
+
+    // Water needs to be taken into account because of marches, swamps and wetlands...
+    /* if (water) {
+      color.multiply(getWaterColor(water));
+    }
+
+    if (fertility < 0.2) {
+      color.lerp(getTemperatureColor(temperature), 1 - fertility * 5);
+    }
+*/
+    return shadeColor2(color.getHexString(), position.y / 20);
+  };
+
+  return (
+    <group position={position} rotation={rotation}>
+      {/*
   <mesh>
         <geometryResource resourceId="robotGeometry" />
         <materialResource resourceId="robotTexture" />
       </mesh>
   */}
-    <mesh>
-      <boxGeometry width={1} height={1} depth={1} />
-      <meshBasicMaterial color={color} wireframe={wireframe} />
-    </mesh>
-  </group>
-);
+      <mesh>
+        <boxGeometry width={width} height={height} depth={depth} />
+        <meshBasicMaterial
+          color={getColor(water, growth, temperature, fertility, position)}
+        />
+      </mesh>
+    </group>
+  );
+};
 
 Cell.propTypes = {
-  position: PropTypes.instanceOf(Vector3),
-  rotation: PropTypes.instanceOf(Euler)
+  position: PropTypes.instanceOf(Vector3).isRequired,
+  rotation: PropTypes.instanceOf(Euler).isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  depth: PropTypes.number.isRequired,
+  isWater: PropTypes.bool.isRequired,
+  water: PropTypes.number.isRequired,
+  fertility: PropTypes.number.isRequired,
+  temperature: PropTypes.number.isRequired,
+  growth: PropTypes.number.isRequired
 };
 
 export default Cell;
--- src/reducers/cellReducer.js
+++ src/reducers/cellReducer.js
@@ -1,28 +1,67 @@
 import THREE, { Vector3, Euler } from "three";
 
-const positionScale = 0.5;
+/*const positionScale = 0.5;
 const positionSpeed = 0.001;
 const positionOffset = 0.2;
 
 const rotationSpeed = 0.02;
 const rotationScale = 0.03;
+*/
+
+const delta = 0.002;
 
 /**
- * We can manage our game state in a series of small, easy to reason about
+ * We can manage our cell state in a series of small, easy to reason about
  * functions
  **/
-export default function cellReudcer(time) {
+export default function cellReudcer(cells, time) {
   // Merge the old state with the updated properties
-  return {
-    cellPosition: new Vector3(
-      0,
-      0,
-      0 //positionScale * Math.sin(time * positionSpeed) + positionOffset
-    ),
-    cellRotation: new Euler(
-      0,
-      0,
-      0 //rotationScale * (time * rotationSpeed)
-    )
-  };
+  return cells.map(cell => {
+    let fertility = cell.fertility;
+    let water = cell.water;
+    let growth = cell.growth;
+
+    /*if (water) {
+      fertility += delta;
+    } else {
+      fertility -= delta;
+    }
+
+    if (cell.temp <= 10) {
+      fertility -= delta;
+    } else if (cell.temp > 10) {
+      fertility += delta;
+    } else if (cell.temp > 30) {
+      fertility += delta / 2;
+      water -= delta / 2;
+    } else if (cell.temp > 50) {
+      fertility -= delta;
+      water -= delta;
+    }
+
+    if (growth) {
+      growth += delta;
+      water -= delta;
+    }
+
+    if (fertility) {
+      growth += delta;
+    } else {
+      growth -= delta;
+    }
+
+    console.log("aaa", water, fertility, growth);*/
+    return {
+      position: cell.position,
+      rotation: cell.rotation,
+      width: cell.width,
+      height: cell.height,
+      depth: cell.depth,
+      temperature: cell.temperature,
+      // water: Math.max(0, Math.min(water, 1)),
+      water: cell.water,
+      fertility: Math.max(0, Math.min(fertility, 1)),
+      growth: Math.max(0, Math.min(growth, 1))
+    };
+  });
 }
